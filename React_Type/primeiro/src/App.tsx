657fc42766d22a8a4968d06dd49afdf9090f25c8
Ranielli Pinho <ranielli.pinho@outlook.com>
2025-05-10 22:21:19 -0300

    praticando

--- React_Type/primeiro/src/App.tsx
+++ React_Type/primeiro/src/App.tsx
@@ -1,19 +1,53 @@
-import { Header } from "./components/header";
-import { Aluno } from "./components/aluno";
-import { Footer } from "./components/footer";
+import { useState } from "react";
+
+interface InfoAlunoProps {
+  nome: string;
+  idade: string;
+}
 
 export default function App() {
+  const [input, setInput] = useState("");
+  const [idade, setIdade] = useState("");
+  const [contador, setContador] = useState(0);
+
+  const [infoAluno, setInfoAluno] = useState<InfoAlunoProps>();
+
+  function mostrarNome() {
+    setInfoAluno({
+      nome: input,
+      idade: idade,
+    });
+  }
+  function somaContador() {
+    setContador(contador + 1);
+  }
+  function subtraiContador() {
+    if (contador === 0) {
+      return;
+    }
+    setContador(contador - 1);
+  }
   return (
     <div>
-      <Header title="Aprendendo do zero ao avançado!" />
+      <h1>Trabahando com UseState</h1>
+      <input
+        placeholder="Digite o nome"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
 
-      <Aluno nome="Ana Caroline" idade={25} />
-
-      <Aluno nome="Matheus Fraga" idade={24} />
-
-      <Aluno nome="Lucas Silva" idade={36} />
-
-      <Footer />
+      <input
+        placeholder="Digite a idade"
+        value={idade}
+        onChange={(e) => setIdade(e.target.value)}
+      />
+      <button onClick={mostrarNome}>Mostrar nome</button>
+      <hr />
+      <h3>Bem vindo: {infoAluno?.nome}</h3>
+      <h4>Idade: {infoAluno?.idade}</h4>
+      <h3>Contador: {contador}</h3>
+      <button onClick={somaContador}>+</button>
+      <button onClick={subtraiContador}>-</button>
     </div>
   );
 }
