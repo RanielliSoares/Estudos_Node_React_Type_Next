f85a87f7dec7a3470362da235e7da92b65d3c57e
Ranielli Pinho <ranielli.pinho@outlook.com>
2025-05-10 22:48:05 -0300

    criação estrutural

--- React_Type/calculadoraCombustivel/src/App.tsx
+++ React_Type/calculadoraCombustivel/src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+import logoImg from "./assets/logo.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <main className="container">
+        <img className="logo" src={logoImg} alt="logo calculadora" />
+        <h1 className="title">Qual melhor opção ?</h1>
+
+        <form className="form">
+          <label>Alcool (preço por litro):</label>
+          <input
+            className="input"
+            type="number"
+            placeholder="4.75"
+            min="1"
+            step="0.01"
+            required
+          />
+          <label>Gasolina (preço por litro):</label>
+          <input
+            className="input"
+            type="number"
+            placeholder="4.75"
+            min="1"
+            step="0.01"
+            required
+          />
+          <input type="submit" value="Calcular" className="button" />
+        </form>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
