2d3a38becccb1c801e0774f3b51f2da440abadf2
Ranielli Pinho <ranielli.pinho@outlook.com>
2025-05-14 13:00:54 -0300

    finalizando projeto

--- React_Type/projetoFrases/src/App.tsx
+++ React_Type/projetoFrases/src/App.tsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+import logoImg from "./assets/logo.png";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [textoFrase, setTextoFrase] = useState("");
+  const [categoriaSelecionada, setCategoriaSelecionada] = useState(0);
+
+  const allFrases = [
+    {
+      id: 1,
+      nome: "Motivação",
+      frases: [
+        "Siga os bons e aprenda com eles.",
+        "O bom-senso vale mais do que muito conhecimento.",
+        "O riso é a menor distância entre duas pessoas.",
+        "Deixe de lado as preocupações e seja feliz.",
+        "Realize o óbvio, pense no improvável e conquiste o impossível.",
+        "Acredite em milagres, mas não dependa deles.",
+        "A maior barreira para o sucesso é o medo do fracasso.",
+      ],
+    },
+    {
+      id: 2,
+      nome: "Bom dia",
+      frases: [
+        "Acordar de bem com a vida é o primeiro passo para ter um dia abençoado! Bom dia, família!",
+        "A melhor forma de acordar é pular da cama e se preparar para correr atrás de todos os seus sonhos! Bom dia, mundo!",
+        "Escreva em seu coração: todo dia é o melhor dia do ano.",
+        "Bom dia! Não se esqueça que a sua alma é o reflexo do sol, tão forte e brilhante quanto um girassol.",
+      ],
+    },
+    {
+      id: 3,
+      nome: "Boa noite",
+      frases: ["Boa noite durma bem", "Teste frase boa noite"],
+    },
+  ];
+
+  function handleSwitchCategory(index: number) {
+    setCategoriaSelecionada(index);
+  }
+
+  function gerarFrase() {
+    let numeroAleatorio = Math.floor(
+      Math.random() * allFrases[categoriaSelecionada].frases.length
+    );
+    setTextoFrase(
+      `"${allFrases[categoriaSelecionada].frases[numeroAleatorio]}"`
+    );
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      <img alt="Logo frases" src={logoImg} className="logo" />
+
+      <h2 className="title">Categorias</h2>
+      <section className="category-area">
+        {allFrases.map((item, index) => (
+          <button
+            key={item.id}
+            className="category-button"
+            style={{
+              borderWidth:
+                item.nome === allFrases[categoriaSelecionada].nome ? 2 : 0,
+              borderColor: "#1fa4db",
+            }}
+            onClick={() => handleSwitchCategory(index)}
+          >
+            {item.nome}
+          </button>
+        ))}
+      </section>
+
+      <button className="button-frase" onClick={gerarFrase}>
+        Gerar frase
+      </button>
+
+      {textoFrase !== "" && <p className="textoFrase">{textoFrase}</p>}
+    </div>
+  );
 }
 
-export default App
+export default App;
