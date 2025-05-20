4916ca65732c6360f7cbd654c1e41aa00a7b241c
Ranielli Pinho <ranielli.pinho@outlook.com>
2025-05-19 21:46:48 -0300

    criação da página Home

---
+++ React_Type/devLink/src/components/Social/index.tsx
@@ -0,0 +1,12 @@
+import type { ReactNode } from "react";
+interface SocialProps {
+  url: string;
+  children: ReactNode;
+}
+export function Social({ children, url }: SocialProps) {
+  return (
+    <a href={url} rel="noopener noreferrer" target="blank">
+      {children}
+    </a>
+  );
+}
