b5bbe94960e1758d3b96ac9d9d483fcb65738f37
Ranielli Pinho <ranielli.pinho@outlook.com>
2025-05-26 23:24:37 -0300

    conditional rendering preview links

--- React_Type/devLink/src/pages/admin/index.tsx
+++ React_Type/devLink/src/pages/admin/index.tsx
@@ -41,16 +41,18 @@ export function Admin() {
               onChange={(e) => setTextColorInput(e.target.value)}
             />
           </div>
+
         </section>
-        <div className="flex items-center justify-center flex-col mb-7 p-1 border-gray-100/25 border rounded-md">
-          <label className="text-white font-medium mt-2 mb-3">Veja como esta ficando:</label>
-          <article className="w-11/12 max-w-lg flex flex-col items-center justify-between rounded px-1 py-3"
-            style={{ marginBottom: 8, marginTop: 8, backgroundColor: backgroundColorInput }}
-          >
-            <p className="font-medium" 
-            style={{ color: textColorInput }}>Canal do Youtube</p>
-          </article>
-        </div>
+        {nameInput !== '' && (
+          <div className="flex items-center justify-center flex-col mb-7 p-1 border-gray-100/25 border rounded-md">
+            <label className="text-white font-medium mt-2 mb-3">Veja como esta ficando:</label>
+            <article className="w-11/12 max-w-lg flex flex-col items-center justify-between rounded px-1 py-3"
+              style={{ marginBottom: 8, marginTop: 8, backgroundColor: backgroundColorInput }}
+            >
+              <p className="font-medium"
+                style={{ color: textColorInput }}>{nameInput}</p>
+            </article>
+          </div>)}
 
         <button type="submit" className="mb-7 bg-blue-600 h-9 rounded-md text-white font-medium gap-4 flex justify-center items-center"
         >Cadastrar</button>
